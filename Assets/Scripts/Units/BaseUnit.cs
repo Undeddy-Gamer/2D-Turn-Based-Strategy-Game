f0dc73db568137f7b5d20c3187caa0914905d0dc
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-04-03 14:52:44 +1100

    added dying to baseunit

--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -12,8 +12,9 @@ public class BaseUnit : MonoBehaviour
     public int baseAttackRange = 1;
     public int baseInitiation = 5;
     public bool canFly = false;
-    
-
+    public float dying = 10f;
+    public Material skin;
+     
     [Space(5), Header("Current Stats")]
     public float currentHealth = 5;
 
@@ -25,6 +26,16 @@ public class BaseUnit : MonoBehaviour
     
     private void Update()
     {
+        if (currentHealth <= 0)
+        {
+            dying -= Time.deltaTime;
+            skin.SetFloat("Fade", dying / 10);
+        }
+        if (dying <= 0)
+        {
+            Destroy(this);
+        }
+        
         //if (TurnManager.TurnEnds >= 0)
         //{
         //    actionPoints = 2;
